f9b97c6e869e4af45fe4311aa5222ee818102f0a
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-03-01 23:08:15 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 //main.cpp
 #include <iostream>
-#ifndef
 #include "personType.h"
-#endif
-#ifndef
 #include "studentType.h"
-#endif
 
 using namespace std;
 
 int main ()
 {
   personType person[2];
-  studenType student[3];
+  studentType student[3];
 
   
   return 0;
