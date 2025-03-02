5d37bdfce0f10ab7ef0713e34005168f049463a6
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-03-01 23:17:21 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,46 @@ int main ()
 {
   personType person[2];
   studentType student[3];
+  
+  string stringtoInput;
+  double doubletoInput;
+  char chartoInput;
+  
+  for (int i =0 ; i < 2; i ++)
+    {
+      cout << "\nInput information for personType object [" << i + 1 <<"]\n";
+      cout << "First Name : ";
+      getline (cin, stringtoInput);
+      setFName (stringtoInput);
+      cout << endl;
 
+      cout << "Last Name : ";
+      getline (cin, stringtoInput);
+      setLName (stringtoInput);
+      cout << endl;
+
+      cout << "Address   : ";
+      getline (cin, stringtoInput);
+      setAddress (stringtoInput);
+      cout << endl;
+
+      cout << "Height : ";
+      cin >> doubletoInput;
+      setHeight ()
+      cin.ignore(doubletoInput);
+      cout << endl;
+      
+      cout << "DOB    : ";
+      getline (cin, stringtoInput);
+      setDOB (stringtoInput);
+      cout << endl;
+      
+      cout << "Gender  : ";
+      cin.get (chartoInput);
+      setDOB (chartoInput);
+      cout << endl;
+    }
+  
   
   return 0;
 }
