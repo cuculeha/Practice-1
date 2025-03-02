e45bd1fcb8cbf2580134c0f27f292318a7e437ad
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-03-01 22:36:32 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
 //main.cpp
+#include <iostream>
+#ifndef
+#include "personType.h"
+#endif
+#ifndef
+#include "studentType.h"
+#endif
+
+using namespace std;
+
+int main ()
+{
+  personType person[2];
+  studenType student[3];
+
+  
+  return 0;
+}
